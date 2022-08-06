0c95a35cd650fef39166dd75b0a8dc3ae3a29c68
Kolyvanov <Kolyvanovsl80@mail.ru>
2022-08-06 23:35:40 +0300

    added demonstration

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        MagicBox <String> boxOfString = new MagicBox<>(5);
+        MagicBox <Number> boxOfNumber = new MagicBox<>(3);
+
+        System.out.println(boxOfString.pick());
+        boxOfNumber.add(1);
+        boxOfNumber.add(2);
+        boxOfNumber.add(3);
+
+        System.out.println(boxOfNumber.add(4));
+
+        System.out.println(boxOfNumber.pick());
 
     }
 }
