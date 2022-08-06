adb7703aabc8f9bdffe8b57a85b28d5ee460df5d
Kolyvanov <Kolyvanovsl80@mail.ru>
2022-08-06 22:30:21 +0300

    added method add

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,11 +1,19 @@
-public class MagicBox <T>{
+public class MagicBox<T> {
     protected int sizeBox;
-    protected T [] items;
+    protected T[] items;
 
-    public MagicBox (int sizeBox) {
+    public MagicBox(int sizeBox) {
         this.sizeBox = sizeBox;
         items = (T[]) new Object[sizeBox];
     }
 
-    
+    public boolean add(T item) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                items[i] = item;
+                return true;
+            }
+        }
+        return false;
+    }
 }
